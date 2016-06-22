6acc37381eb0f55d53728291f02d1ae59635c689
peba02 <peter.bakonyi@betssongroup.com>
2016-06-22 14:03:06 +0200

    feat(create-sample): improve watermarking strategy

--- src/create-sample.js
+++ src/create-sample.js
@@ -2,21 +2,62 @@
 
 const gm = require('gm');
 
+const getImageSize = require('./get-image-size');
+
+// NOTE: this function adds the watermark once to the top center of the image
+//       other watermarking strategy can be added if needed,
+//       just return another array with this format
+function getDefaultWatermarkDrawings(sourceSize, watermarkSize, watermarkPath) {
+  if (sourceSize.width < watermarkSize.width || sourceSize.heigth < watermarkSize.height) {
+    throw new Error('Watermark cannot be larger than the source image');
+  }
+
+  const x0 = sourceSize.width * 0.2;
+  const y0 = sourceSize.height * 0.05;
+  const w = sourceSize.width * 0.6;
+  const h = w * watermarkSize.height / watermarkSize.width;
+
+  // image operator x0,y0 w,h filename
+  return [`image over ${x0},${y0} ${w},${h} "${watermarkPath}"`];
+}
+
+/**
+ * @param {string} sourcePath
+ * @param {string} destinationPath
+ * @param {string} [watermarkPath=null]
+ * @param {number} [quality=100] 0-100
+ * @param {{width: [number], height: [number]}} [newSize]
+ */
 module.exports = ({
   sourcePath,
   destinationPath,
-  watermarkPath,
-  quality = 99
+  watermarkPath = null,
+  quality = 100,
+  newSize = null
 }) => new Promise((resolve, reject) => {
-  gm(sourcePath)
-    .draw([`image over 0,0 0,0 "${watermarkPath}"`])
-    .quality(quality)
-    .write(destinationPath, (err) => {
-      if (err) {
-        reject(err);
-        return;
+  getImageSize(watermarkPath ? [sourcePath, watermarkPath] : [sourcePath])
+    .then(sizes => {
+      let s = gm(sourcePath);
+
+      if (watermarkPath) {
+        s = s.draw(getDefaultWatermarkDrawings(sizes[0], sizes[1], watermarkPath));
       }
 
-      resolve(destinationPath);
+      if (quality) {
+        s = s.quality(quality);
+      }
+
+      if (newSize) {
+        s = s.resize(newSize.width || null, newSize.height || undefined);
+      }
+
+      s.write(destinationPath, (err) => {
+        if (err) {
+          reject(err);
+          return;
+        }
+
+        resolve(destinationPath);
+      });
     });
 });
--- test/create-sample.js
+++ test/create-sample.js
@@ -10,8 +10,34 @@ describe('createSample', () => {
     return createSample({
       sourcePath: tu.beachJpg,
       watermarkPath: tu.watermarkPng,
-      destinationPath: tu.getOutputImagePath('create-sample.jpg'),
-      quality: 99
+      destinationPath: tu.getOutputImagePath('create-sample-watermark-200height.jpg'),
+      quality: 99,
+      newSize: {
+        height: 200
+      }
+    });
+  });
+
+  it('should not add a watermark when omitted', () => {
+    return createSample({
+      sourcePath: tu.beachJpg,
+      destinationPath: tu.getOutputImagePath('create-sample-no-watermark-400width.jpg'),
+      quality: 99,
+      newSize: {
+        width: 400
+      }
+    });
+  });
+
+  it('should set the quality correctly', () => {
+    return createSample({
+      sourcePath: tu.beachJpg,
+      watermarkPath: tu.watermarkPng,
+      destinationPath: tu.getOutputImagePath('create-sample-small-poor-quality.jpg'),
+      quality: 50,
+      newSize: {
+        height: 100
+      }
     });
   });
 });
